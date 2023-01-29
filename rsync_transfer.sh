789237bece274a3741e71017ce9edb92eefff50a
SpaceinvaderOne <54743094+SpaceinvaderOne@users.noreply.github.com>
2023-01-29 19:50:45 +0000

    Create rsync_transfer.sh

---
+++ rsync_transfer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Set the source and destination directories
+src_dir="/path/to/source/directory"
+dst_dir="/path/to/destination/directory"
+
+# Define the option to delete the source directory and its contents after a successful transfer (defualt no) set to "yes" to remove source
+delete_src="no"
+
+# Use rsync to copy the contents of the source directory to the destination directory
+rsync -ah --progress --stats $src_dir/ $dst_dir/
+
+# Check if the rsync was successful by checking the exit status
+if [ $? -eq 0 ]; then
+  echo "The contents of the directory have been successfully copied."
+  
+  # Check if the delete_src option is set to "yes"
+  if [ "$delete_src" == "yes" ]; then
+    # Delete the source directory and its contents
+    rm -rf $src_dir
+    echo "The source directory and its contents have been deleted."
+  fi
+else
+  echo "An error occurred while copying the contents of the directory."
+fi
